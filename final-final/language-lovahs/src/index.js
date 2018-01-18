8d8c003d0f89797f99f0297bf2dab6c5c33da950
github username <github email address>
2018-01-18 13:34:58 -0500

    fixed semantic

--- final-final/language-lovahs/src/index.js
+++ final-final/language-lovahs/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
