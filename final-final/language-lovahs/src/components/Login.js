b6c2e844b3d2ba85db85e9b4e31a38dffaf30fa7
RochelLevi <rochlevi@gmail.com>
2018-01-19 09:07:06 -0500

    added login markup

--- final-final/language-lovahs/src/components/Login.js
+++ final-final/language-lovahs/src/components/Login.js
@@ -1,9 +1,43 @@
 import React from 'react'
 
-const Login = () => {
+const Login = (props) => {
   return(
-    <h1>Login!</h1>
-  )
+  <div class="ui middle aligned center aligned grid">
+    <div class="column">
+
+      <h2 class="ui image header">
+        <div class="content">
+          Log-in to your account
+        </div>
+      </h2>
+
+      <form class="ui large form">
+        <div class="ui stacked secondary segment">
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="user icon"></i>
+              <input type="text" name="email" placeholder="E-mail address"/>
+            </div>
+          </div>
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="lock icon"></i>
+              <input type="password" name="password" placeholder="Password"/>
+            </div>
+          </div>
+          <div class="ui fluid large blue submit button">Login</div>
+        </div>
+
+        <div class="ui error message"></div>
+
+      </form>
+
+      <div class="ui message">
+        New to us? <a href="/register">Register</a>
+      </div>
+    </div>
+  </div>
+)
 }
 
 export default Login
