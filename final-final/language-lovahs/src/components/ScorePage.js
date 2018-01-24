1db76d778689c55b4d124d71d5ad80e96e39b477
RochelLevi <rochlevi@gmail.com>
2018-01-24 09:09:33 -0500

    working on game score

---
+++ final-final/language-lovahs/src/components/ScorePage.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const ScorePage = (props) => {
+
+  return (
+
+    <div class="ui middle aligned center aligned grid">
+      <div class="column">
+        <div class="ui stacked secondary segment">
+        <h2 class="ui image header">
+          <div as="h4" class="content">
+              Congratulations!
+          </div>
+        </h2>
+          <div class="content">
+            <h1 class="ui header">
+            You learned {props.score} words this round!
+          </h1>
+          </div>
+
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default ScorePage
